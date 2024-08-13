6d7820ecce83518f49aa42b105952abefd23f214
Despina Misheva <despina.miseva@gmail.com>
2024-08-13 17:20:10 +0200

    Update app.js

--- apps/prvtest/app.js
+++ apps/prvtest/app.js
@@ -3,9 +3,9 @@ const APP_ID = 'prvtest';
 // Function to send "Hello World" over BLE
 function sendHelloWorld() {
   let data = [0x48, 0x65, 0x6c, 0x6c, 0x6f, 0x20, 0x57, 0x6f, 0x72, 0x6c, 0x64]; // "Hello World" in ASCII
-  NRF.setAdvertising([{
-    0xFF: data
-  }], { showName: false });
+
+  NRF.setAdvertising(data, { showName: false});
+  
   console.log("Hello World sent over BLE");
 }
 
