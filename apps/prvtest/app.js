ac2a26741a8ec2f3919515407bf067650adfee4e
Despina Misheva <despina.miseva@gmail.com>
2024-08-13 17:04:18 +0200

    Update app.js

--- apps/prvtest/app.js
+++ apps/prvtest/app.js
@@ -1,12 +1,5 @@
 const APP_ID = 'prvtest';
 
-const settings = Object.assign(readJSON('metadata.json',1)||{});
-
-let mainMenu = {
-  "": { "title": "--  Hello  --" },
-  "Send Hello World": function() { sendHelloWorld(); }
-};
-
 // Function to send "Hello World" over BLE
 function sendHelloWorld() {
   let data = [0x48, 0x65, 0x6c, 0x6c, 0x6f, 0x20, 0x57, 0x6f, 0x72, 0x6c, 0x64]; // "Hello World" in ASCII
