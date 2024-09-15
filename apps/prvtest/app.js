74174fa3ff4207823b78024bfe572e8336e1c8f2
Despina Misheva <despina.miseva@gmail.com>
2024-09-15 12:13:30 +0200

    Update app.js

--- apps/prvtest/app.js
+++ apps/prvtest/app.js
@@ -1,10 +1,30 @@
-g.clear();
-g.setFont('Vector', 20);
-g.drawString("Hello World aaaaaa!");
+let isNewHrmData = false;
+let hrmTimestamp = null;
+let hrm = { bpm: 0, confidence: 0 }; 
 
-Bluetooth.println("");
-      Bluetooth.println(JSON.stringify({
-        poraka: "Majka ti da ja"
+Bangle.setHRMPower(true);
+
+Bangle.on('HRM', function(newHrm) {
+  hrm = newHrm;
+  isNewHrmData = true;
+  hrmTimestamp = new Date().toISOString(); 
+
+  g.clear();
+  g.setFont('Vector', 20);
+  g.drawString(`HR: ${hrm.bpm}`, 10, 40); 
+  g.drawString(`Time: ${hrmTimestamp}`, 10, 80);
+});
+
+function transmitUpdatedSensorData() {
+  Bluetooth.println("");
+  Bluetooth.println(JSON.stringify({
+    bpm: hrm.bpm,       
+    timestamp: hrmTimestamp 
   }));
 
-//pobogu te molam proraboti 
+  if (isNewHrmData) {
+    isNewHrmData = false;
+  }
+}
+
+setInterval(transmitUpdatedSensorData, 20000);
