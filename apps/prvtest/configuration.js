933a77439898fa6586d3f56b521a654c96bc049c
Despina Misheva <despina.miseva@gmail.com>
2024-09-22 13:50:51 +0200

    Create configuration.js

---
+++ apps/prvtest/configuration.js
@@ -0,0 +1,6 @@
+let highBpmThreshold = 100; 
+let cooldownDurationBPM = 60 * 1000; 
+let cooldownDurationFall = 90 * 1000;
+let confirmationTimeoutDuration = 30 * 1000;
+let timeoutDuration = 15 * 1000;
+let fallThreshold = 2.1; 
